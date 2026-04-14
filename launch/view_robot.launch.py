0c35c4bd21830e4aba8e8eb89a33f20568ecdb7a
kheyal <piyaskheyal@gmail.com>
2026-04-15 01:37:12 +0600

    model and robot view launch file is improted

---
+++ launch/view_robot.launch.py
@@ -0,0 +1,45 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+
+from launch_ros.actions import Node
+import xacro
+
+def generate_launch_description():
+    robotXacroName='robot_arm_urdf'
+    namePackage='robot_arm_description'
+    modelFileRelativePath='model/robot_arm_urdf.xacro'
+    pathModelFile=os.path.join(get_package_share_directory(namePackage),modelFileRelativePath)
+    
+    robotDescription=xacro.process_file(pathModelFile).toxml()
+    
+    nodeRobotStatePublisher=Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[
+            {'robot_description': robotDescription, 'use_sim_time': True},
+        ],
+    )
+
+    nodeRobotJointStatePublisher=Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen',
+    )
+
+    rvizConfigFile=os.path.join(get_package_share_directory(namePackage),'config/rviz_robot_arm.rviz')
+    nodeRviz2=Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        arguments=['-d', rvizConfigFile],
+    )
+
+
+    launchDescriptionObject=LaunchDescription()
+    launchDescriptionObject.add_action(nodeRobotStatePublisher)
+    launchDescriptionObject.add_action(nodeRobotJointStatePublisher)
+    launchDescriptionObject.add_action(nodeRviz2)
+
+    return launchDescriptionObject
