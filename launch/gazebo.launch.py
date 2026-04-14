2cad78804a5db9add91d4bb13dcebcda96d62697
kheyal <piyaskheyal@gmail.com>
2026-04-15 02:00:50 +0600

    the robot is spawning in the gazebo simulation now

---
+++ launch/gazebo.launch.py
@@ -0,0 +1,73 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, RegisterEventHandler
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch.event_handlers import OnProcessExit
+import xacro
+
+def generate_launch_description():
+    pkg_name = 'robot_fd'
+    pkg_share = get_package_share_directory(pkg_name)
+
+    # Process URDF file
+    urdf_file = os.path.join(pkg_share, 'model', 'robot_arm_urdf.xacro')
+    doc = xacro.process_file(urdf_file)
+    robot_description = {'robot_description': doc.toxml()}
+
+    # Robot State Publisher
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='both',
+        parameters=[robot_description, {"use_sim_time": True}]
+    )
+
+    # Gazebo Sim
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={'gz_args': '-r empty.sdf'}.items()
+    )
+
+    # Spawn Robot
+    spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        arguments=['-topic', 'robot_description',
+                   '-name', 'robot_arm',
+                   '-z', '0.0']
+    )
+
+    # Joint State Broadcaster
+    joint_state_broadcaster = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+    )
+
+    # Arm Controller
+    arm_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["arm_controller"],
+    )
+
+    # Gripper Controller
+    gripper_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["gripper_controller"],
+    )
+
+    return LaunchDescription([
+        gazebo,
+        robot_state_publisher,
+        spawn_entity,
+        joint_state_broadcaster,
+        arm_controller,
+        gripper_controller,
+    ])
